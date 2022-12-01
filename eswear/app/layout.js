35abc65ea650e3edb5eb43247e1f06e74a958524
saltfans <aff.davis@gmail.com>
2022-12-01 06:08:47 +0100

    headlessui

--- eswear/app/layout.js
+++ eswear/app/layout.js
@@ -1,8 +1,19 @@
+import Link from 'next/link';
+import { Fragment } from 'react'
+
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <head />
-      <body>{children}</body>
-    </html>
+  <html>
+    <head />
+    <body>
+      <Popover className="relative bg-white">
+        
+          <Link href="/">Home</Link>
+          <Link href="/notifications">Notifications</Link>
+          <Link href="/swipe">Swipe for Match</Link>
+      </Popover>
+      <div>{children}</div>
+    </body>
+  </html>
   )
 }
