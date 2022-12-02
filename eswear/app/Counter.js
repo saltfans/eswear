f0aa5c47a4fee37019aa07e8eab0d6b6af56969e
saltfans <aff.davis@gmail.com>
2022-12-03 00:42:34 +0100

    upgrade

--- eswear/app/Counter.js
+++ eswear/app/Counter.js
@@ -2,6 +2,7 @@
 //CLIENT COMPONENTS
 // imports for Popover dropdown ... menu!
 import { useState } from 'react';
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/solid';
 
 import Link from 'next/link';
 import { Popover } from '@headlessui/react';
@@ -9,7 +10,10 @@ import { Popover } from '@headlessui/react';
 export default function Counter() {
     return (
     <Popover className='relative'>
-        <Popover.Button className='text-red-500'>More</Popover.Button>
+        <Popover.Button className='flex justify-start'>
+            <EllipsisHorizontalIcon className="h-6 w-6 pr-1"/>
+            <span className='pl-1'>More</span>
+        </Popover.Button>
         <Popover.Panel className='absolute z-10m w-48 bg-red-300'>
             <div className=''>
                 <div className=''>
--- eswear/app/layout.js
+++ eswear/app/layout.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import '../styles/globals.css';
 import { Fragment } from 'react';
 import Image from 'next/legacy/image';
-
+import { HomeIcon, BellAlertIcon, HeartIcon, PlusIcon, ChatBubbleBottomCenterTextIcon, EllipsisHorizontalIcon } from '@heroicons/react/24/solid'
 // Counter use for client side! japievieno bilde kas bus profila bilde.
 import Counter from './Counter';
 export default function RootLayout({ children }) {
@@ -10,21 +10,51 @@ export default function RootLayout({ children }) {
   <html>
     <head />
     <body class='bg-gradient-to-r from-gray-500 via-purple-500 to-pink-500'>
-       <nav class='w-48 h-full bg-black/40 shadow-3xl border-r-xl border-white-500'>
+       <div class='w-48 h-full bg-black/40 shadow-3xl backdrop-blur-sm'>
 
-        <div class="logo">
+        <div class="w-full h-auto pl-4 grid items-center justify-items-center border-b-2 border-pink-600">
           <Image src='/others/manifuns.png' alt='me' width='192' height='192' />
         </div>
 
+        <nav class='px-6 mt-2 text-blue-300'>
+          <ul>
+            <li class='rounded border-2 p-1 mb-2 border-pink-500 w-36 h-10'>
+              <Link href="/" className='flex justify-start'>
+                <HomeIcon className="h-6 w-6 pr-1"/>
+                <span className='pl-1'>Home</span>
+              </Link>
+            </li>
+            <li class='rounded border-2 p-1 mb-2 border-pink-500 w-36 h-10'>
+              <Link href="/notifications" className='flex justify-start'>
+                <BellAlertIcon className="h-6 w-6 pr-1"/>
+                <span className='pl-1'>Notifications</span>
+              </Link>
+            </li>
+            <li class='rounded border-2 p-1 mb-2 border-pink-500 w-36 h-10'>
+              <Link href="/swipe" className='flex justify-start'>
+                <HeartIcon className="h-6 w-6 pr-1"/>
+                <span className='pl-1'>Swipe</span>
+              </Link>
+            </li>
+            <li class='rounded border-2 p-1 mb-2 border-pink-500 w-36 h-10'>
+              <Link href="/post" className='flex justify-start'>
+                <PlusIcon className="h-6 w-6 pr-1"/>
+                <span className='pl-1'>Create Post</span>
+              </Link>
+            </li>
+            <li class='rounded border-2 p-1 mb-2 border-pink-500 w-36 h-10'>
+              <Link href="messages" className='flex justify-start'>
+                <ChatBubbleBottomCenterTextIcon className="h-6 w-6 pr-1"/>
+                <span className='pl-1'>Messages</span>
+              </Link>
+            </li>
 
-        <Link href="/">Home</Link>
-        <Link href="/notifications">Notifications</Link>
-        <Link href="/swipe">Swipe for match</Link>
-        <Link href="/post">Create Post</Link>
-        <Link href="messages">Messages</Link>
-        
-        <Counter />
-      </nav> 
+            <li class='rounded border-2 p-1 mb-2 border-pink-500 w-36 h-10'>
+              <Counter/>
+            </li>
+          </ul>
+        </nav>
+      </div> 
 
       <div>{children}</div>
     </body>
