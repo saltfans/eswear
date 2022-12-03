7a75d567e952624d1a14ea953c606754f3f5336f
saltfans <aff.davis@gmail.com>
2022-12-04 00:58:58 +0100

    up

--- eswear/app/layout.js
+++ eswear/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <body class='bg-gradient-to-r from-gray-500 via-purple-500 to-pink-500'>
       
     <div>
-      <div className='container hidden mx-auto grid grid-cols-2 border-b-2 border-sky-500'>
+      <div className='container mx-auto grid grid-cols-2 border-b-2 border-sky-500'>
         <div className=' ' >
             <Image src='/others/manifuns.png' height='350' width='350' className='' />
         </div>
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
       </div>
     </div>
 
-    <div class="container grid grid-cols-6 gap-x-2 gap-y-3 m-auto px-4">
+    <div class="container hidden grid grid-cols-6 gap-x-2 gap-y-3 m-auto px-4">
        <div class='w-full min-w-48 h-full bg-black/40 shadow-3xl backdrop-blur-sm row-span-3'>
 
         <div class="w-full h-auto pl-4 grid items-center justify-items-center border-b-2 border-pink-600">
