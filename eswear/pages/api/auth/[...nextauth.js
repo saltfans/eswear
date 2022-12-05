8c6d621e8ebdd25f9894e974131f11992413ca2a
saltfans <aff.davis@gmail.com>
2022-12-05 04:20:45 +0100

    update

--- eswear/pages/api/auth/[...nextauth.js
+++ eswear/pages/api/auth/[...nextauth.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -8,6 +9,10 @@ export const authOptions = {
     FacebookProvider({
         clientId: process.env.FACEBOOK_CLIENT_ID,
         clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
       }),
   ],
 }
