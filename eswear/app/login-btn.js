ab75e8d690a9a1198ed204bb8fecdd9873782a72
saltfans <aff.davis@gmail.com>
2022-12-06 05:00:33 +0100

    test

--- eswear/app/login-btn.js
+++ eswear/app/login-btn.js
@@ -4,27 +4,23 @@ import UserInformation from "../components/user-information";
 export default function Component({children}) {
   const { data: session } = useSession();
   if (session) {
-    return (
-      <>
-        <script>
-            document.getElementById("homepage").classlist.add("hidden");
-            document.getElementById("homepage2").classlist.remove("hidden");
-        </script>
+    return 
+        document.getElementById("homepage").classlist.add("hidden");
+        document.getElementById("homepage2").classlist.remove("hidden");
+       ( <>
         Signed in as {session.user.email} <br />
         <UserInformation data={session.user} />
         <button onClick={() => signOut()}>Sign out</button>
         {children}
-      </>
-    )
+        </>)
+    
   }
-  return (
-    <>
-        <script>
-            document.getElementById("homepage2").classlist.add("hidden");
-            document.getElementById("homepage").classlist.remove("hidden");
-        </script>
+  return
+        document.getElementById("homepage2").classlist.add("hidden");
+        document.getElementById("homepage").classlist.remove("hidden");
+        (<>
       Not signed in <br />
       <button onClick={() => signIn()}>Sign in</button><br />
-    </>
+        </>
   );
 }
