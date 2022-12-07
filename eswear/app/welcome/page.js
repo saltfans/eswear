c003e9e8ad1c2ec8d1e34f27501ff93e6b9d2fda
saltfans <aff.davis@gmail.com>
2022-12-07 18:48:38 +0100

    test

--- eswear/app/welcome/page.js
+++ eswear/app/welcome/page.js
@@ -1,5 +1,4 @@
 import Image from 'next/legacy/image';
-import { HeartIcon, GiftIcon, HandThumbUpIcon, HandThumbDownIcon, FireIcon, ArrowUturnLeftIcon } from '@heroicons/react/24/solid'
 // app/page.js
 // This file maps to the index route (/)
 export default function Page() {
@@ -11,10 +10,10 @@ export default function Page() {
         <div id="homepage" class="">
           <div className='container mx-auto  grid grid-cols-2 border-b-2 border-sky-500'>
             <div className=' ' >
-                <Image src='/others/manifuns.png' height='350' width='350' className='' />
+                <Image src='../others/manifuns.png' height='350' width='350' className='' />
             </div>
             <div className='float-left m-6'>
-              <LoginBtn />
+              
             </div>
           </div>
           <div>
