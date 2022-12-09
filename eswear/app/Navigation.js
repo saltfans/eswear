cf9b49fb4f8bf98cf119931f7ad0ae2b63342385
saltfans <aff.davis@gmail.com>
2022-12-09 02:44:42 +0100

    top

--- eswear/app/Navigation.js
+++ eswear/app/Navigation.js
@@ -5,38 +5,26 @@ import Link from 'next/link';
 
 const Navigation = () => {
   const Menus = [
-    { name: "Home", href: "/", icon: <HomeIcon />, dis: "translate-x-0" },
-    { name: "News", href: "/notifications", icon: <BellAlertIcon />, dis: "translate-x-16" },
-    { name: "Swipe", href: "/swipe", icon: <HeartIcon />, dis: "translate-x-32" },
-    { name: "Chat", href: "/messages", icon: <ChatBubbleBottomCenterTextIcon />, dis: "translate-x-48" },
-    { name: "More", href: "#", icon: <EllipsisHorizontalIcon />, dis: "translate-x-64" },
+    { name: "Home", href: "/", icon: <HomeIcon className="h-4 w-4 mx-2"/> },
+    { name: "News", href: "/notifications", icon: <BellAlertIcon className="h-4 w-4 mx-2"/>},
+    { name: "Swipe", href: "/swipe", icon: <HeartIcon className="h-4 w-4 mx-2"/> },
+    { name: "Chat", href: "/messages", icon: <ChatBubbleBottomCenterTextIcon className="h-4 w-4 mx-2"/>},
+    { name: "More", href: "#", icon: <EllipsisHorizontalIcon className="h-4 w-4 mx-2"/> },
   ];
   const [active, setActive] = useState(0);
   return (
-    <div className="bg-white max-h-[4.4rem] px-6 rounded-t-xl">
+    <div className="bg-white px-6 rounded-t-xl">
       <ul className="flex relative">
-        <span
-          className={`bg-rose-600 duration-500 ${Menus[active].dis} border-4 border-gray-900 h-16 w-16 absolute
-         -top-5 rounded-full`}
-        >
-          <span
-            className="w-3.5 h-3.5 bg-transparent absolute top-4 -left-[18px] 
-          rounded-tr-[11px] shadow-myShadow1"
-          ></span>
-          <span
-            className="w-3.5 h-3.5 bg-transparent absolute top-4 -right-[18px] 
-          rounded-tl-[11px] shadow-myShadow2"
-          ></span>
-        </span>
-        {Menus.map((menu, i) => (
-          <li key={i} className="w-16">
+    
+        {Menus.map(({ name, href, icon }) => (
+          <li key={name} className="w-16">
             <Link href={href}>
             <a className="flex flex-col text-center pt-6" onClick={() => setActive(i)}>
               <span className={`text-xl cursor-pointer duration-500 ${ i === active && "-mt-6 text-white" }`}>
-                {menu.icon}
+                {icon}
               </span>
-              <span className={` ${ active === i ? "translate-y-4 duration-700 opacity-100" : "opacity-0 translate-y-10" } `}>
-                {menu.name}
+              <span className="translate-y-4 duration-700 opacity-100">
+                {name}
               </span>
             </a>
             </Link>
