196c75c9c7b4b3cc958aa4f13893b7cb100cd09f
saltfans <aff.davis@gmail.com>
2022-12-10 04:03:33 +0100

    shesh

--- eswear/app/page.js
+++ eswear/app/page.js
@@ -7,15 +7,16 @@ export default function Page() {
         <div className="w-full">
             <div className="w-full bg-white/50 h-42 flex">
                 <Image src='/others/profile2.jpg' alt='me' width='64' height='64' className='rounded-full float-left' />
-                <div>
+                <div className='pl-4'>
                 <storng>Username</storng><br/>
                 <span>Level : 1</span>
                 </div>
+                <div>
+                    <p>5 min ago...</p>
+                    <p>Icon menu for copy link or report</p>
+                </div>
             </div>
-            <div>
-                <p>5 min ago...</p>
-                <p>Icon menu for copy link or report</p>
-            </div>
+            
         </div>
         <div className="w-full">
             <div>
