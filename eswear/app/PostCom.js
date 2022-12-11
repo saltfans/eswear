8946370c99c82954e74b292d76a8d6705c9559dd
saltfans <aff.davis@gmail.com>
2022-12-11 22:26:20 +0100

    vv

--- eswear/app/PostCom.js
+++ eswear/app/PostCom.js
@@ -11,13 +11,13 @@ const PostComments = () => {
     return (
         <div className='w-full relative bg-red-500 px-4 py-2'>
                 <div className='rounded-lg bg-slate-500 flex w-full mx-1 px-1 py-1 my-0.5'>
-                    <Image src='/others/profile1.jpg' width='26' height='26' className='rounded-full'/>
+                    <Image src='/others/profile1.jpg' width='26' height='26' className='rounded-full h-26 w-26'/>
                     <span className='text-[10px] font-serif font-light tracking-tight pr-2 text-left'>Admin</span>
                     <div className=''>
                         <p>Man jau patiik!!</p>
                     </div>
                     <div className='flex relative w-full text-right align-text-top'>
-                        <div className='float-right pr-2 pt-2 flex-initial absolute right-0 top-0 flex flex-row-reverse'>
+                        <div className='float-right flex-initial absolute right-0 top-0 flex flex-row-reverse'>
                             <EllipsisVerticalIcon className='h-5 w-5 text-emerald-300'/>
                             <p className='text-xs font-serif font-light tracking-tight text-right leading-3 text-emerald-300'>5 min ago.</p>
                         </div>
