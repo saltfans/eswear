fd499d62b017b4039625e39360e954747c1515ed
saltfans <aff.davis@gmail.com>
2022-12-11 22:05:49 +0100

    ll

--- eswear/app/PostCom.js
+++ eswear/app/PostCom.js
@@ -7,8 +7,8 @@ import Image from 'next/legacy/image';
 
 const PostComments = () => {
     return (
-        <div className='w-full bg-black/20'>
-                <div className='rounder-lg bg-slate-800/40 w-full mx-1 px-1 py-1 my-0.5'>
+        <div className='w-full bg-red-500'>
+                <div className='rounder-lg bg-slate-500 w-full mx-1 px-1 py-1 my-0.5'>
                     <Image src='/others/profile1.jpg' width='26' height='26' className='rounded-full'/>
                     Admin 
                 </div>
