822eefa2408cc2a7a0961291a4f7e49f099b2dd1
saltfans <aff.davis@gmail.com>
2022-12-11 18:38:07 +0100

    sos

--- eswear/app/page.js
+++ eswear/app/page.js
@@ -7,14 +7,14 @@ export default function Page() {
     <div>
         <div className="w-full">
             <div className="w-full bg-black/40  flex">
-                <div className='p-2'>
+                <div className='pl-2 pt-2 pb-2'>
                 <Image src='/others/profile2.jpg' alt='me' width='54' height='54' className='rounded-full float-left' />
                 </div>
-                <div className='pl-4'>
+                <div className='pl-1 pt-2'>
                 <storng className='text-sm font-sans font-medium text-left'>Username</storng><br/>
                 <span className='text-sm font-serif font-light tracking-tight text-left text-zinc-800'>Level : 1</span>
                 </div>
-                <div className='float-right pr-2'>
+                <div className='float-right pr-2 pt-2'>
                     <p className='text-sm font-sans font-medium text-right'>Icon</p>
                     <p className='text-sm font-serif font-light tracking-tight text-right'>5 min ago.</p>
                 </div>
