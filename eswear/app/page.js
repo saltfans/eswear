dadf6ff813e08e46076e597ca10c7b5b729fdcde
saltfans <aff.davis@gmail.com>
2022-12-11 19:47:12 +0100

    yo

--- eswear/app/page.js
+++ eswear/app/page.js
@@ -10,11 +10,11 @@ export default function Page() {
                 <div className='pl-2 pt-2 pb-2 flex-initial'>
                 <Image src='/others/profile2.jpg' alt='me' width='54' height='54' className='rounded-full float-left' />
                 </div>
-                <div className='pl-1 pt-2 flex-initial'>
-                <storng className='text-sm font-sans leading-3 font-medium text-left'>Username</storng><br/>
-                <span className='text-sm font-serif font-light leading-3 tracking-tight text-left text-black-400'>Level : 1</span>
+                <div className='pl-1 pt-2 flex-initial align-text-top text-left'>
+                <p className='text-sm font-sans leading-3 font-medium text-left'>Username</p><br/>
+                <p className='text-sm font-serif font-light leading-3 tracking-tight text-left text-black-400'>Level : 1</p>
                 </div>
-                <div className='flex-none relative w-full text-right align-text-top'>
+                <div className='flex relative w-full text-right align-text-top'>
                 <div className='float-right pr-2 pt-2 flex-initial absolute right-0 top-0 flex flex-row-reverse'>
                     <EllipsisVerticalIcon className='h-5 w-5'/>
                     <p className='text-sm font-serif font-light tracking-tight text-right leading-3 text-black-400'>5 min ago.</p>
