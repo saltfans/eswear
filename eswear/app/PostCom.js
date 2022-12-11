3959c917ac40c196d995028683a6122463a6fff2
saltfans <aff.davis@gmail.com>
2022-12-11 22:12:29 +0100

    nn

--- eswear/app/PostCom.js
+++ eswear/app/PostCom.js
@@ -10,7 +10,7 @@ const PostComments = () => {
         <div className='w-full relative bg-red-500 px-4 py-2'>
                 <div className='rounded-lg bg-slate-500 w-full mx-1 px-1 py-1 my-0.5'>
                     <Image src='/others/profile1.jpg' width='26' height='26' className='rounded-full'/>
-                    Admin 
+                    <span className='text-[10px] font-serif font-light tracking-tight pr-2 text-left'>Admin</span> 
                 </div>
         </div>
     )
