45fe700841dfe0c0ead9c917692b459b3bb9f2cb
saltfans <aff.davis@gmail.com>
2022-12-11 20:08:12 +0100

    bb

--- eswear/app/page.js
+++ eswear/app/page.js
@@ -12,12 +12,12 @@ export default function Page() {
                 </div>
                 <div className='pl-1 pt-2 flex-initial align-text-top'>
                 <storng className='text-sm font-sans text-emerald-100 leading-3 font-medium text-left'>Username</storng><br/>
-                <p className='text-sm font-serif font-light leading-3  text-left text-emerald-300'>Level : 1</p>
+                <p className='text-xs font-serif font-light leading-3  text-left text-emerald-300'>Level : 1</p>
                 </div>
                 <div className='flex relative w-full text-right align-text-top'>
                 <div className='float-right pr-2 pt-2 flex-initial absolute right-0 top-0 flex flex-row-reverse'>
                     <EllipsisVerticalIcon className='h-5 w-5 text-emerald-300'/>
-                    <p className='text-sm font-serif font-light tracking-tight text-right leading-3 text-emerald-300'>5 min ago.</p>
+                    <p className='text-xs font-serif font-light tracking-tight text-right leading-3 text-emerald-300'>5 min ago.</p>
                 </div>
                 </div>
             </div>
