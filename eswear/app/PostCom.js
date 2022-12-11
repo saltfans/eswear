4f2bb2a16f0558c093ea5418348ef038810ee71a
saltfans <aff.davis@gmail.com>
2022-12-11 21:53:37 +0100

    ss

--- eswear/app/PostCom.js
+++ eswear/app/PostCom.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from 'next/legacy/image';
+
 
 // app/page.js
 // This file maps to the index route (/)
@@ -7,7 +9,8 @@ const PostComments = () => {
     return (
         <div className='w-full'>
                 <div className='rounder-lg bg-slate-800/40 w-full px-1 py-0.5'>
-
+                    <Image src='/others/profile1.jpg' width='20' height='20'/>
+                    Admin 
                 </div>
         </div>
     )
