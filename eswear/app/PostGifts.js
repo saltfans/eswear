6b7a8167733b2b36eddc4650adf500df8aaf493f
saltfans <aff.davis@gmail.com>
2022-12-13 00:37:43 +0100

    dd

--- eswear/app/PostGifts.js
+++ eswear/app/PostGifts.js
@@ -11,25 +11,33 @@ const PostGifts = () => {
         <div className='w-full relative bg-black/20 px-4 py-2'>
                 <div className='rounded-lg bg-slate-500 flex w-full mx-1 px-1 py-1 my-0.5'>
                     <div className='flex-initial'>
-                        <div>
-                            <Image src='/others/roses/rose-red.png' width='50' height='50'/><br/>
-                            <span>Red Roses</span>
-                            <span>Price : 1.5€</span>
+                        <div className='flex items-left'>
+                            <Image src='/others/roses/rose-red.png' width='50' height='50'/>
+                            <div>
+                                <strong>Red Roses</strong>
+                                <span>Price : 1.5€</span>
+                            </div>
                         </div>
-                        <div>    
+                        <div className='flex items-left'>   
                             <Image src='/others/roses/rose-blue-2.png' width='50' height='50'/>
-                            <span>Blue Roses</span>
-                            <span>Price : 7€</span>
+                            <div>
+                                <strong>Blue Roses</strong>
+                                <span>Price : 7€</span>
+                            </div>
                         </div>
-                        <div>
+                        <div className='flex items-left'> 
                             <Image src='/others/roses/rose-purple.png' width='50' height='50'/>
-                            <span>Purple Roses</span>
-                            <span>Price : 5€</span>
+                            <div>
+                                <strong>Purple Roses</strong>
+                                <span>Price : 5€</span>
+                            </div>
                         </div>
-                        <div>
+                        <div className='flex items-left'> 
                             <Image src='/others/roses/rose-yellow.png' width='50' height='50'/>
-                            <span>Yellow Roses</span>
-                            <span>Price : 2.5€</span>
+                            <div>
+                                <strong>Yellow Roses</strong>
+                                <span>Price : 2.5€</span>
+                            </div>
                         </div>
                     </div>
                 </div>
