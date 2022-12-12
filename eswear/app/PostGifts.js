684ce9992160a9ccafce3ef4346f3def5d06b5fa
saltfans <aff.davis@gmail.com>
2022-12-13 00:26:31 +0100

    mm

--- eswear/app/PostGifts.js
+++ eswear/app/PostGifts.js
@@ -11,18 +11,26 @@ const PostGifts = () => {
         <div className='w-full relative bg-black/20 px-4 py-2'>
                 <div className='rounded-lg bg-slate-500 flex w-full mx-1 px-1 py-1 my-0.5'>
                     <div className='flex-initial'>
-                        <Image src='/others/roses/rose-red.png' width='50' height='50'/><br/>
-                        <span>Red Roses</span><br/>
-                        <span>Price : 1.5€</span>
-                        <Image src='/others/roses/rose-blue-2.png' width='50' height='50'/>
-                        <span>Blue Roses</span>
-                        <span>Price : 7€</span>
-                        <Image src='/others/roses/rose-purple.png' width='50' height='50'/>
-                        <span>Purple Roses</span>
-                        <span>Price : 5€</span>
-                        <Image src='/others/roses/rose-yellow.png' width='50' height='50'/>
-                        <span>Yellow Rose s</span>
-                        <span>Price : 2.5€</span>
+                        <div>
+                            <Image src='/others/roses/rose-red.png' width='50' height='50'/><br/>
+                            <span>Red Roses</span>
+                            <span>Price : 1.5€</span>
+                        </div>
+                        <div>    
+                            <Image src='/others/roses/rose-blue-2.png' width='50' height='50'/>
+                            <span>Blue Roses</span>
+                            <span>Price : 7€</span>
+                        </div>
+                        <div>
+                            <Image src='/others/roses/rose-purple.png' width='50' height='50'/>
+                            <span>Purple Roses</span>
+                            <span>Price : 5€</span>
+                        </div>
+                        <div>
+                            <Image src='/others/roses/rose-yellow.png' width='50' height='50'/>
+                            <span>Yellow Roses</span>
+                            <span>Price : 2.5€</span>
+                        </div>
                     </div>
                 </div>
         </div>
