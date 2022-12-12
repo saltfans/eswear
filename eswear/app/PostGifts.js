1a528e35c4f1f4996b51f9aad9795868ecccc657
saltfans <aff.davis@gmail.com>
2022-12-13 00:54:49 +0100

    up

--- eswear/app/PostGifts.js
+++ eswear/app/PostGifts.js
@@ -11,31 +11,35 @@ const PostGifts = () => {
         <div className='w-full relative bg-black/20 px-4 py-2'>
                 <div className='w-full rounded-lg bg-slate-500 flex w-full mx-1 px-1 py-1 my-0.5'>
                     <div className=''>
-                        <div className='flex-initial'>
+                        <div className='flex'>
                             <Image src='/others/roses/rose-red.png' width='50' height='50'/>
                             <div className='flex-initial pl-1'>
                                 <strong className='text-sm leading-3 text-left'>Red Roses</strong>
+                                <br/>
                                 <span className='text-xs leading-3 text-left'>Price : 1.5€</span>
                             </div>
                         </div>
-                        <div className='flex-initial'>   
+                        <div className='flex'>   
                             <Image src='/others/roses/rose-blue-2.png' width='50' height='50'/>
                             <div className='flex-initial pl-1'>
                                 <strong className='text-sm leading-3 text-left'>Blue Roses</strong>
+                                <br/>
                                 <span className='text-xs leading-3 text-left'>Price : 7€</span>
                             </div>
                         </div>
-                        <div className='flex-initial'> 
+                        <div className='flex'> 
                             <Image src='/others/roses/rose-purple.png' width='50' height='50'/>
                             <div className='flex-initial pl-1'>
                                 <strong className='text-sm leading-3 text-left'>Purple Roses</strong>
+                                <br/>
                                 <span className='text-xs leading-3 text-left'>Price : 5€</span>
                             </div>
                         </div>
-                        <div className='flex-initial'> 
+                        <div className='flex'> 
                             <Image src='/others/roses/rose-yellow.png' width='50' height='50'/>
                             <div className='flex-initial pl-1'>
                                 <strong className='text-sm leading-3 text-left'>Yellow Roses</strong>
+                                <br/>
                                 <span className='text-xs leading-3 text-left'>Price : 2.5€</span>
                             </div>
                         </div>
