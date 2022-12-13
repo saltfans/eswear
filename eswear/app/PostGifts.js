a8b40aa0340f9e793c7fcfa65d739e1038cb07f3
saltfans <aff.davis@gmail.com>
2022-12-13 01:44:31 +0100

    ff

--- eswear/app/PostGifts.js
+++ eswear/app/PostGifts.js
@@ -10,15 +10,15 @@ import { CheckBadgeIcon } from '@heroicons/react/24/solid';
 const PostGifts = () => {
     return (
         <div className='w-full relative bg-black/20 px-4 py-2'>
-                <div className='w-full rounded-lg bg-slate-500 flex w-full mx-1 my-0.5'>
+                <div className='w-full rounded-lg bg-black/10 flex w-full mx-1 my-0.5'>
                     <div className='flex flex-wrap gap-3'>
                         <label className='cursor-pointer'>
                             <input type='radio' className='peer sr-only' name='priceing'/>
                             <div className='w-48 mx-w-xl p-2 
-                            bg-white text-gray-600 rounded-md 
+                            bg-black/30 text-gray-600 rounded-md 
                             hover:shadow ring-2 ring-transparent 
-                            peer-checked:text-sky-600 
-                            peer-checked:ring-sky-400 
+                            peer-checked:text-blue-300 
+                            peer-checked:ring-pink-500 
                             peer-checked:ring-offset-2'>
                                 <div className='flex flex-col gap-1'>
                                     <div className='flex items-center'>
@@ -31,7 +31,7 @@ const PostGifts = () => {
                                         </div>
                                     </div>
                                     <div className='flex items-end justify-between'>
-                                        <p><span className='text-sm font-bold'>Texts</span> Texts cits</p>
+                                        <p><span className='text-sm font-bold'>Šeit ir</span> Texts :0</p>
                                         <p className='text-xs font-bold'>1,5€ / One</p>
                                     </div>
                                 </div>
