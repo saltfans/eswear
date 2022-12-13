61f926bae21d13333ea085f2a7bd09255a7fbaf7
saltfans <aff.davis@gmail.com>
2022-12-13 04:30:56 +0100

    upgrade

--- eswear/app/right-one.js
+++ eswear/app/right-one.js
@@ -1,10 +1,46 @@
 import Image from 'next/legacy/image';
-import { HeartIcon } from '@heroicons/react/24/solid'
+import { HeartIcon } from '@heroicons/react/24/solid';
+
 // app/page.js
 // This file maps to the index route (/)
 export default function Page() {
     return (
-        <div class='grid items-center justify-items-center mt-8'>
+        <div class='grid place-items-center justify-items-center mt-2'>
+            <div className='p-4 w-full text-white'>
+                <div className='flex justify-between items-center'>
+                    <Image src='/others/profile1.jpg' alt='me' width='40' height='40' className='rounded-full' />
+                    <div>
+                        <div className='grid'>
+                            <span className='font-thin text-lg'>Post</span>
+                            <span className='font-semibold text-lg'>23</span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='grid'>
+                            <span className='font-thin text-lg'>Followers</span>
+                            <span className='font-semibold text-lg'>790</span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='grid'>
+                            <span className='font-thin text-lg'>Subscribers</span>
+                            <span className='font-semibold text-lg'>217</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='pt-5'>
+                    <div className='grid'>
+                        <span className='font-semibold text-lg'>texts</span>
+                        <span className='font-thin text-lg'>texts3222</span>
+                    </div>
+                </div>
+                <div className='pt-5'>
+                    <div className='grid'>
+                        <span className='font-semibold'>Mission</span>
+                        <span className='font-thin'>uzdevums seit raksturots</span>
+                    </div>
+                </div>
+            </div>
             <ul>
                 <li>Line 1-3 Profile pic on left.</li>
                 <li>Line 1 Balance € on right.</li>
