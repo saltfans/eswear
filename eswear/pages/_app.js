03e55d5d9c6c6ee7eddb258a0b0341f4640d0906
saltfans <aff.davis@gmail.com>
2022-12-16 02:10:35 +0100

    test

--- eswear/pages/_app.js
+++ eswear/pages/_app.js
@@ -1,15 +1,7 @@
-import '../styles/globals.css'
-import { useState } from 'react'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  const [supabase] = useState(() => createBrowserSupabaseClient())
-
-  return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-      <Component {...pageProps} />
-    </SessionContextProvider>
-  )
+  return <Component {...pageProps} />
 }
+
 export default MyApp
