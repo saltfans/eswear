321c19705cc7bbd136feab7eb56d1d2a9a760a8e
saltfans <aff.davis@gmail.com>
2022-12-17 01:00:25 +0100

    back

--- eswear/pages/api/auth/[...nextauth].js
+++ eswear/pages/api/auth/[...nextauth].js
@@ -2,23 +2,19 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
-import { FirebaseAdapter } from "@next-auth/firebase-adapter";
-import { Firestore } from "firebase/firestore";
-import { db } from "../../../firebase.config";
-import * as firestoreFunctions from "firebase/firestore";
-export default NextAuth({
+export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     // ...add more providers here
+    FacebookProvider({
+        clientId: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+      }),
       GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET
       }),
   ],
-  adapter: FirebaseAdapter({
-    db : db,
-    ...firestoreFunctions,
-  }),
-
   secret: "LlKq6ZtYbCx",
-})
+}
+export default NextAuth(authOptions)
