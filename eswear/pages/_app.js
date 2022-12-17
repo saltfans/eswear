26bf2d3d4e9590cf795b9cf56bbff23bee6065fa
saltfans <aff.davis@gmail.com>
2022-12-17 04:41:42 +0100

    go

--- eswear/pages/_app.js
+++ eswear/pages/_app.js
@@ -2,25 +2,7 @@ import '../styles/globals.css';
 import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return (
-   <>
-    <Script 
-      strategy='afterInteractive' 
-      src='https://www.googletagmanager.com/gtag/js?id=G-DTVRSLR2T0'
-    />
-    <Script id='google-analytics' strategy='afterInteractive'>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-DTVRSLR2T0');
-        `}
-      </Script>
-    <Component {...pageProps} />
-
-   </> 
-  )
-  
+  return <Component {...pageProps} />
 }
 
 export default MyApp
