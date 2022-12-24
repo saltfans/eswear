576ca2ec4f503827b0240bad793cfd406644e9af
saltfans <aff.davis@gmail.com>
2022-12-24 01:38:55 +0100

    go

--- eswear/app/auth/page.js
+++ eswear/app/auth/page.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Login from "./Login"
+import { UserIcon } from '@heroicons/react/24/solid'
 export default function LoginPage() {
     return (
         <div>
             <Login />
+            <UserIcon  className="w-8 h-8 "/>
         </div>
     )
 }
