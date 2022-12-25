8b9aa695be9048a4100762d729e5dcb9ce88abf2
saltfans <aff.davis@gmail.com>
2022-12-25 15:40:58 +0100

    yohoho

--- eswear/pages/_app.js
+++ eswear/pages/_app.js
@@ -1,19 +1,14 @@
-import '../styles/globals.css';
-
+import '../styles/globals.css'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  // Create a new supabase browser client on every first render.
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+  const [supabase] = useState(() => createBrowserSupabaseClient())
 
   return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
       <Component {...pageProps} />
     </SessionContextProvider>
   )
 }
+export default MyApp
