d2f2236c23540952010906549df20cb9b5f31f9e
saltfans <aff.davis@gmail.com>
2022-12-25 17:06:26 +0100

    og

---
+++ eswear/app/auth/head.js
@@ -0,0 +1,12 @@
+export default function Head() {
+    return (
+      <>
+        <title>SaltFans</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://raw.githubusercontent.com/supabase/supabase/master/examples/user-management/nextjs-ts-user-management/styles/globals.css
+" />
+      </>
+    )
+  }
+  
