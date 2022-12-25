a2157908c6e41d8b3a9299f0e805c6f0849276bc
saltfans <aff.davis@gmail.com>
2022-12-25 15:42:34 +0100

    GO

--- eswear/pages/_app.js
+++ eswear/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import { useState } from 'react'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 
